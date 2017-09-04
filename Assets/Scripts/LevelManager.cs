730b13c66f3ea647c9279ca6f8af3bea7ba8817b
Arthur BABEY <git@ababey.com>
2017-09-04 15:45:18 +0200

    Menu

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour 
+{
+    public void LoadLevel(string name) 
+    {
+        SceneManager.LoadScene(name);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Ne me quittes pas ...");
+    }
+	
+}
