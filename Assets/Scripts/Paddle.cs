2977ff8b7492c928e059ae480059c3d6d1d4ff0a
Arthur BABEY <git@ababey.com>
2017-09-09 14:11:03 +0200

    Move paddle with mouse

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+
+    static float minX = 0.5f;
+    static float maxX = 15.5f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float mousePosInBlocks = Input.mousePosition.x / Screen.width * 16;
+
+        Vector3 paddlePos = new Vector3(
+            Mathf.Clamp(mousePosInBlocks, minX, maxX), 
+            transform.position.y
+        );
+
+        transform.position = paddlePos;
+	}
+}
