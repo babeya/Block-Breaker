a49ccc37c43156e8eadb9c4136589de9e2343408
Arthur BABEY <git@ababey.com>
2017-09-09 14:32:20 +0200

    Lauch ball from the paddle

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class Ball : MonoBehaviour 
+{
+    public Paddle paddle;
+
+    Vector3 paddleToBallVector;
+    bool hasStarted = false;
+
+	// Use this for initialization
+	void Start () 
+    {
+        paddleToBallVector = transform.position - paddle.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if(!hasStarted) 
+        {
+            transform.position = paddle.transform.position + paddleToBallVector;    
+        }
+
+        if (Input.GetMouseButtonDown(0)) 
+        {
+            hasStarted = !hasStarted;
+            Rigidbody2D body = GetComponent<Rigidbody2D>();
+
+            body.velocity = new Vector2(2f, 10f);
+        }
+	}
+}
